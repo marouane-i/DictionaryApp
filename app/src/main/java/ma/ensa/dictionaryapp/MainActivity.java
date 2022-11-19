c310f15c08d11a77d8595ec0b9d27b7aaa47b1a9
marouane <marwanezibout10@gmail.com>
2022-11-19 18:18:48 +0100

    adding bottom menu & home page fragment

--- app/src/main/java/ma/ensa/dictionaryapp/MainActivity.java
+++ app/src/main/java/ma/ensa/dictionaryapp/MainActivity.java
@@ -1,14 +1,50 @@
 package ma.ensa.dictionaryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ActivityManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+import java.net.NoRouteToHostException;
+
+import ma.ensa.dictionaryapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity{
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // this is to hide the top bar guys
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId()==R.id.home){
+                replaceFragment(new HomeFragment());
+            }else if(item.getItemId()==R.id.settings){
+                replaceFragment(new SettingsFragment());
+            }else if(item.getItemId()==R.id.favorite){
+                replaceFragment(new FavoriteFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
