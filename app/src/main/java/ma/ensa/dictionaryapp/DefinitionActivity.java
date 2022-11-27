191f126ea357bd0eaa77e8a05ec485d729654327
marouane <marwanezibout10@gmail.com>
2022-11-27 19:14:12 +0100

    update

--- app/src/main/java/ma/ensa/dictionaryapp/DefinitionActivity.java
+++ app/src/main/java/ma/ensa/dictionaryapp/DefinitionActivity.java
@@ -1,10 +1,27 @@
 package ma.ensa.dictionaryapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import ma.ensa.dictionaryapp.model.Word;
 
@@ -14,13 +31,19 @@ public class DefinitionActivity extends AppCompatActivity {
     private TextView description;
     private TextView example;
     private TextView updesc;
+    private ImageView fav;
+    private Word word;
+    private ArrayList<Word> favoriteList;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "ResourceAsColor"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_definitions);
-        Word word = (Word) getIntent().getSerializableExtra("word");
+        word = (Word) getIntent().getSerializableExtra("word");
 
         String updesString = "Definition of "
                 +word.getId()+" "+word.getDefinitions().get(0).getType()
@@ -32,6 +55,7 @@ public class DefinitionActivity extends AppCompatActivity {
         description = findViewById(R.id.nameDefinition1);
         example = findViewById(R.id.example1);
         updesc = findViewById(R.id.updesc);
+        fav = findViewById(R.id.imageViewFav);
 
         id.setText(word.getId());
         type.setText(word.getDefinitions().get(0).getType());
@@ -42,5 +66,33 @@ public class DefinitionActivity extends AppCompatActivity {
             example.setText(word.getDefinitions().get(0).getExamples().get(0).getExample());
         }
         updesc.setText(updesString);
+        loadData();
+    }
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("courses", null);
+        Type type = new TypeToken<ArrayList<Word>>() {}.getType();
+        favoriteList = gson.fromJson(json, type);
+        if (favoriteList == null) {
+            favoriteList = new ArrayList<>();
+        }
+    }
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(favoriteList);
+        editor.putString("words", json);
+        editor.apply();
+        loadData();
+        Toast.makeText(this, "This word is saved in favorite ", Toast.LENGTH_SHORT).show();
+    }
+    public void doFavorite(View view){
+        System.out.println("clicked");
+        fav.setImageResource(R.drawable.ic_baseline_favorite_true_24);
+        word.setFavorite(true);
+        favoriteList.add(word);
+        saveData();
     }
 }
